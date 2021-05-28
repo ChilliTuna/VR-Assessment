cb91cf402e07cd2046dd5c8641b3629e9034cb9b
Tom <karzacinc@gmail.com>
2021-05-28 16:49:03 +1000

    Base NavMesh Setup
     + Basic Navigation Mesh added.
     + Basic Navigation Agent added.

---
+++ VR project/Assets/TomStuff/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public Transform end;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.SetDestination(end.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
