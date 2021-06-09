35edf2a6281e58ab748f104399244a563199ca08
Connor <connormccaskie@gmail.com>
2021-06-09 17:24:13 +1000

    fixed the hand material
    thanks tom

---
+++ Assets/Scripts/HandMaterialSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMaterialSetter : MonoBehaviour
+{
+    public string leftHandName;
+    public string rightHandName;
+
+    public Material handMaterial;
+
+    private void Start()
+    {
+        InvokeRepeating("SetMaterial", 0, 0.2f);
+    }
+
+    void SetMaterial()
+    {
+        GameObject.Find(leftHandName).GetComponentInChildren<SkinnedMeshRenderer>().material = handMaterial;
+        GameObject.Find(rightHandName).GetComponentInChildren<SkinnedMeshRenderer>().material = handMaterial;
+        if (GameObject.Find(leftHandName).GetComponentInChildren<SkinnedMeshRenderer>().material == handMaterial && GameObject.Find(rightHandName).GetComponentInChildren<SkinnedMeshRenderer>().material == handMaterial)
+        {
+            CancelInvoke("SetMaterial");
+        }
+    }
+}
